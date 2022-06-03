89777d7eef2df9700a1db519fc334e9dee419895
tysonOliveira <thaisonoliveira@gmail.com>
2022-06-03 18:09:09 -0300

    module.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Header } from './components/Header';
 import { Post } from './Post';
 
+import styles from './App.module.css';
+
 import './global.css';
 
 export function App() {
@@ -8,15 +10,21 @@ export function App() {
     <div>
       <Header />
 
-      <Post 
-        author="Tyson Oliveira" 
-        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum nesciunt earum     placeat, sapiente velit voluptatibus officiis ducimus! Aliquam maiores eligendi sequi autem molestiae, sapiente incidunt excepturi eum accusantium corporis! Minus!" 
-      />
-
-      <Post 
-        author="Thais"
-        content="Um novo post"
-      />
+      <div className={styles.wrapper} >
+        <aside>
+          sidebar
+        </aside>
+        <main>
+          <Post
+            author="Tyson Oliveira"
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur pariatur quos ea dolorum! Laboriosam, ex iusto, quidem amet totam molestiae accusamus eius ab, earum ipsa consectetur expedita at? Consequuntur, expedita?"
+          />
+          <Post
+            author="Oliveira"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa tenetur excepturi, incidunt placeat harum voluptatibus deleniti enim. Ipsam, facere qui cum magnam aliquid aliquam corporis dolore pariatur sed officiis. Hic."
+          />
+        </main>    
+      </div>
     </div>
   )
 }
