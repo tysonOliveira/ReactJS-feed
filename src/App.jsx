32808833d3d3c11724e2b5464f84f0fbfda08741
tysonOliveira <thaisonoliveira@gmail.com>
2022-06-06 23:58:28 -0300

    create post

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Header } from './components/Header';
-import { Post } from './Post';
+import { Post } from './components/Post';
+import { Sidebar } from './components/Sidebar';
 
 import styles from './App.module.css';
 
@@ -12,7 +13,7 @@ export function App() {
 
       <div className={styles.wrapper} >
         <aside>
-          sidebar
+          <Sidebar />
         </aside>
         <main>
           <Post
