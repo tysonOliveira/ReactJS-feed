c4a20e94fdc72caff33d2f57fbd123ec338d4b91
tysonOliveira <thaisonoliveira@gmail.com>
2022-06-14 00:00:58 -0300

    adjustment-l-date-fns

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,5 @@
 import { format } from 'date-fns';
-import { ptBR } from 'date-fns/locale/pt-BR'
+import ptBR from 'date-fns/locale/pt-BR'
 
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
