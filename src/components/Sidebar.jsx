36df3111c7205505378ae0b38ad9ad87ca3be400
tysonOliveira <thaisonoliveira@gmail.com>
2022-09-22 18:06:44 -0300

    changed-avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,7 @@ export function Sidebar() {
       <img className={styles.cover} src="https://images.unsplash.com/photo-1517180102446-f3ece451e9d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=550&q=50" />
 
       <div className={styles.profile} > 
-        <Avatar src="https://github.com/RafaelPrincival.png" />
+        <Avatar src="https://github.com/tysonOliveira.png" />
 
         <strong>Tyson Oliveira</strong>
         <span>Web Developer</span>
